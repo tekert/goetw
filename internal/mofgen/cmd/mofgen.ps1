90991e9cbfa337100cf03a41abbf2bd951009870
tekert <tekert@gmail.com>
2025-09-07 06:21:10 -0300

    Some ia changes to dump MOF data into a file to be parsed, this a great tool for getting old mof classes per system.

--- internal/mofgen/cmd/mofgen.ps1
+++ internal/mofgen/cmd/mofgen.ps1
@@ -5,32 +5,29 @@
 # Set strict error handling
 $ErrorActionPreference = 'Stop'
 
-# The core function to generate a MOF representation for a single CIM class.
-function Get-MofRepresentation {
+# Stage 1: Generates a simple, unformatted MOF representation for a single class.
+function Get-MofRepresentationRaw {
     param(
         [string]$ClassName,
         [string]$Namespace = 'root\wmi'
     )
 
     $class = Get-CimClass -Namespace $Namespace -ClassName $ClassName -ErrorAction Stop
+    $outputLines = [System.Collections.Generic.List[string]]::new()
 
-    # --- Class Header Formatting ---
-    $classQualifiersString = ($class.CimClassQualifiers | ForEach-Object {
+    # --- Class Header ---
+    $classQualifiers = ($class.CimClassQualifiers | ForEach-Object {
             $qualifierString = $_.Name
-            if ($_.Value) {
+            # Fix: Check for null instead of just truthy values
+            if ($null -ne $_.Value) {
                 if ($_.Name -eq "EventType") {
-                    # Handle single value vs array for EventType
-                    if ($_.Value -is [array]) {
-                        $qualifierString += "{" + ($_.Value -join ", ") + "}"
-                    }
-                    else {
-                        $qualifierString += '(' + $_.Value + ')'
-                    }
+                    $qualifierString += if ($_.Value -is [array]) { "{" + ($_.Value -join ", ") + "}" } else { "(" + $_.Value + ")" }
+                }
+                elseif ($_.Name -eq "EventVersion") {
+                    $qualifierString += "(" + $_.Value + ")"
                 }
                 elseif ($_.Name -eq "dynamic") {
-                    if ($_.Value -is [bool] -and $_.Value) {
-                        $qualifierString += ":ToInstance"
-                    }
+                    if ($_.Value -is [bool] -and $_.Value) { $qualifierString += ":ToInstance" }
                 }
                 elseif ($_.Value -is [string]) {
                     $qualifierString += '("' + $_.Value + '")'
@@ -42,33 +39,19 @@ function Get-MofRepresentation {
             $qualifierString
         }) -join ", "
 
-    $classHeader = ""
-    if ($classQualifiersString) {
-        $classHeader += "[" + $classQualifiersString + "] "
-    }
+    $classHeader = if ($classQualifiers) { "[${classQualifiers}] " } else { "" }
     $classHeader += "class " + $ClassName
-
-    $fullHeader = $classHeader
     if ($class.CimSuperClassName) {
-        $fullHeader += " : " + $class.CimSuperClassName
+        $classHeader += " : " + $class.CimSuperClassName
     }
-    $fullHeader += " {"
+    $classHeader += " {"
+    $outputLines.Add($classHeader)
 
-    if ($fullHeader.Length -gt 90 -and $class.CimSuperClassName) {
-        $output = $classHeader + "`n    : " + $class.CimSuperClassName + " {"
-    }
-    else {
-        $output = $fullHeader
-    }
-
-    # Filter for properties with WmiDataId, but also include all properties of the base MSNT_SystemTrace class
+    # --- Properties ---
     $propertiesToInclude = $class.CimClassProperties | Where-Object {
         $_.Qualifiers['WmiDataId'] -or $ClassName -eq 'MSNT_SystemTrace'
     }
-
-    $sortedProperties = $propertiesToInclude | Sort-Object {
-        if ($_.Qualifiers['WmiDataId']) { [int]$_.Qualifiers['WmiDataId'].Value } else { 0 }
-    }
+    $sortedProperties = $propertiesToInclude | Sort-Object { if ($_.Qualifiers['WmiDataId']) { [int]$_.Qualifiers['WmiDataId'].Value } else { 0 } }
 
     foreach ($property in $sortedProperties) {
         $qualifierObjects = [System.Collections.Generic.List[object]]::new()
@@ -78,26 +61,16 @@ function Get-MofRepresentation {
             $qualifierObjects.Add([pscustomobject]@{ Name = 'format'; Value = 'x' })
         }
 
-        # Custom sort order: WmiDataId first, read last, others in between alphabetically.
-        $sortedQualifiers = $qualifierObjects | Sort-Object -Property @{Expression = {
-                switch ($_.Name) {
-                    'WmiDataId' { 0 }
-                    'read' { 2 }
-                    default { 1 }
-                }
-            }
-        }, Name
+        # Corrected sort order logic
+        $sortOrder = @{ 'WmiDataId' = 0; 'read' = 2 }
+        $sortedQualifiers = $qualifierObjects | Sort-Object -Property @{Expression = { if ($sortOrder.ContainsKey($_.Name)) { $sortOrder[$_.Name] } else { 1 } } }, Name
 
         $hasWmiDataId = $null -ne $property.Qualifiers['WmiDataId']
-
         $propertyQualifiersList = ($sortedQualifiers | ForEach-Object {
-                # Filter out inherited or implicit qualifiers
-                if ($hasWmiDataId -and ($_.Name -in @('Values', 'ValueMap', 'DefineValues'))) { return }
-                if ($_.Name -in @('EmbeddedObject')) { return }
-
-                $qualifierString = $_.Name
+                if (($hasWmiDataId -and ($_.Name -in @('Values', 'ValueMap', 'DefineValues'))) -or $_.Name -eq 'EmbeddedObject') { return }
                 if ($_.Value -is [bool] -and $_.Value -eq $false) { return }
 
+                $qualifierString = $_.Name
                 if ($_.Value -and ($_.Value -isnot [bool])) {
                     if ($_.Value -is [array]) {
                         $qualifierString += '{"' + ($_.Value -join '", "') + '"}'
@@ -112,162 +85,169 @@ function Get-MofRepresentation {
                 $qualifierString
             })
 
-        $cimType = $property.CimType.ToString()
-        $dataType = switch ($cimType) {
-            "SInt8" { "sint8" }
-            "UInt8" { "uint8" }
-            "SInt16" { "sint16" }
-            "UInt16" { "uint16" }
-            "SInt32" { "sint32" }
-            "UInt32" { "uint32" }
-            "SInt64" { "sint64" }
-            "UInt64" { "uint64" }
-            "String" { "string" }
-            "Real32" { "real32" }
-            "Real64" { "real64" }
-            "DateTime" { "datetime" }
-            "Boolean" { "boolean" }
-            "Char16" { "char16" }
-            "Object" { "object" }
-            "Instance" { "object" }
-            Default { $cimType }
-        }
-        if ($dataType.EndsWith("Array")) {
-            $dataType = $dataType.Substring(0, $dataType.Length - 5)
-        }
-
-        # --- Conditional Line Breaking Logic ---
-        $lineLengthLimit = 90
-        $propertyDefinition = " " + $dataType + " " + $property.Name + ";"
-        $singleLineQualifiers = "[ " + ($propertyQualifiersList -join ", ") + " ]"
-        $fullSingleLine = "  " + $singleLineQualifiers + $propertyDefinition
-
-        if ($fullSingleLine.Length -le $lineLengthLimit) {
-            $output += "`n" + $fullSingleLine
-        }
-        else {
-            # Multi-line block format with special handling for large array qualifiers
-            $qualifiersBlock = "[`n"
-            $qualifierStringsForBlock = @()
-
-            foreach ($qualifier in $sortedQualifiers) {
-                # Re-format the qualifier string, but this time with multi-line support for specific arrays
-                if ($hasWmiDataId -and ($qualifier.Name -in @('Values', 'ValueMap', 'DefineValues'))) { continue }
-                if ($qualifier.Name -in @('EmbeddedObject')) { continue }
-                if ($qualifier.Value -is [bool] -and $qualifier.Value -eq $false) { continue }
-
-                $qualifierString = $qualifier.Name
-                if ($qualifier.Value -and ($qualifier.Value -isnot [bool])) {
-                    if ($qualifier.Value -is [array]) {
-                        $itemsPerLine = switch ($qualifier.Name) {
-                            'DefineValues' { 1 }
-                            'Values'       { 2 }
-                            'ValueMap'     { 4 }
-                            default        { 0 }
-                        }
-                        if ($itemsPerLine -gt 0) {
-                            $arrayValue = $qualifier.Value
-                            $lines = [System.Collections.Generic.List[string]]::new()
-                            for ($i = 0; $i -lt $arrayValue.Count; $i += $itemsPerLine) {
-                                $end = [math]::Min($i + $itemsPerLine - 1, $arrayValue.Count - 1)
-                                $lineItems = $arrayValue[$i..$end]
-                                $quotedItems = $lineItems | ForEach-Object { '"{0}"' -f $_ }
-                                $lines.Add(($quotedItems -join ', '))
-                            }
-                            $indent = "      " # 6 spaces
-                            $qualifierString += "{`n$indent  " + ($lines -join ",`n$indent  ") + "`n$indent}"
-                        }
-                        else {
-                            $qualifierString += '{"' + ($qualifier.Value -join '", "') + '"}'
-                        }
-                    }
-                    elseif ($qualifier.Value -is [string]) {
-                        $qualifierString += '("' + $qualifier.Value + '")'
-                    }
-                    else {
-                        $qualifierString += '(' + $qualifier.Value + ')'
-                    }
-                }
-                $qualifierStringsForBlock += $qualifierString
-            }
-            $qualifiersBlock += "    " + ($qualifierStringsForBlock -join ",`n    ")
-            $qualifiersBlock += "`n  ]"
-            $output += "`n  " + $qualifiersBlock + $propertyDefinition
-        }
+        $dataType = $property.CimType.ToString().Replace("Array", "")
+        $qualifiersString = "[ " + ($propertyQualifiersList -join ", ") + " ]"
+        $outputLines.Add("  " + $qualifiersString + " " + $dataType.ToLower() + " " + $property.Name + ";")
     }
 
-    $output += "`n};"
-    return $output
+    $outputLines.Add("};")
+    return $outputLines -join "`n"
 }
+
+# Stage 2: Formats a raw MOF string with wrapping and indentation rules. (Asked IA for this)
+function Format-MofContent {
+    param([string]$MofContent)
+
+    $lineLengthLimit = 90
+    $formattedLines = [System.Collections.Generic.List[string]]::new()
+    $blocks = $MofContent -split '(?=^\[.*\]\s*(?:abstract\s+)?class|^class)'
+
+    foreach ($block in $blocks) {
+        if ($block.Trim().Length -eq 0) { continue }
+        $lines = $block.Trim() -split "`n"
+        $header = $lines[0]
+        $properties = $lines[1..($lines.Count - 2)]
+        $footer = $lines[-1]
+
+        # --- 1. Format Class Header ---
+        # Only break if the line actually exceeds the limit
+        if ($header.Length -gt $lineLengthLimit) {
+            # Find the last comma before the limit
+            $pos = $header.LastIndexOf(',', $lineLengthLimit)
+            if ($pos -gt 0) {
+                # Break at that comma and continue with remaining line
+                $firstPart = $header.Substring(0, $pos + 1)
+                $secondPart = $header.Substring($pos + 1).TrimStart()
+                $header = $firstPart + "`n" + (" " * 9) + $secondPart
+            }
+        }
+
+        # --- 2. Handle empty classes ---
+        if ($properties.Count -eq 0) {
+            $header = $header -replace ' {$', '{}'
+            $formattedLines.Add($header)
+            $formattedLines.Add("")
+            continue
+        }
+        $formattedLines.Add($header)
+
+        # --- 3. Format Properties ---
+        foreach ($prop in $properties) {
+            # Special case: MSNT_SystemTrace.Flags
+            if ($header.Contains("class MSNT_SystemTrace") -and $prop.Contains("] uint32 Flags;")) {
+                $prop -match '(\s*)\[ (.*) \] (.*)' | Out-Null
+                $indent, $qualifiersStr, $rest = $Matches[1], $Matches[2], $Matches[3]
+
+                $newProp = "$indent[`n"
+
+                # Extract and format each qualifier type
+                if ($qualifiersStr -match 'DefineValues({.*?})') {
+                    $items = $Matches[1] -replace '[{}"]' -split ',' | ForEach-Object { $_.Trim() } | Where-Object { $_ }
+                    $newProp += "$indent    DefineValues{`"" + ($items -join "`",`n$indent                 `"") + "`"},`n"
+                }
+
+                if ($qualifiersStr -match 'format\("(.*?)"\)') {
+                    $newProp += "$indent    format(`"$($Matches[1])`"),`n"
+                }
+
+                if ($qualifiersStr -match 'Values({.*?})') {
+                    $items = $Matches[1] -replace '[{}"]' -split ',' | ForEach-Object { $_.Trim() } | Where-Object { $_ }
+                    $newProp += "$indent    Values{`""
+                    for ($i = 0; $i -lt $items.Count; $i += 2) {
+                        if ($i -gt 0) { $newProp += ",`n$indent           `"" }
+                        $newProp += ($items[$i..([Math]::Min($i + 1, $items.Count - 1))] -join "`", `"")
+                    }
+                    $newProp += "`"},`n"
+                }
+
+                if ($qualifiersStr -match 'ValueMap({.*?})') {
+                    $items = $Matches[1] -replace '[{}"]' -split ',' | ForEach-Object { $_.Trim() } | Where-Object { $_ }
+                    $newProp += "$indent    ValueMap{`""
+                    for ($i = 0; $i -lt $items.Count; $i += 4) {
+                        if ($i -gt 0) { $newProp += ",`n$indent             `"" }
+                        $newProp += ($items[$i..([Math]::Min($i + 3, $items.Count - 1))] -join "`", `"")
+                    }
+                    $newProp += "`"}`n"
+                }
+
+                $newProp = $newProp.TrimEnd(",`n") + "`n$indent] $rest"
+                $formattedLines.Add($newProp)
+                continue
+            }
+
+            # Regular properties: only wrap if really long
+            if ($prop.Length -gt $lineLengthLimit) {
+                $prop -match '(\s*)\[(.*)\](.*)' | Out-Null
+                $indent, $qualifiers, $rest = $Matches[1], $Matches[2].Trim(), $Matches[3]
+
+                $formattedLines.Add("$indent[`n$indent  " + ($qualifiers -replace ', ', ",`n$indent  ") + "`n$indent]$rest")
+            }
+            else {
+                $formattedLines.Add($prop)
+            }
+        }
+
+        $formattedLines.Add($footer)
+        $formattedLines.Add("")
+    }
+
+    return ($formattedLines -join "`n").Trim()
+}
+
 
 # --- Main Script Logic ---
 
 $namespace = 'root\wmi'
 $baseClassName = 'MSNT_SystemTrace'
-$outputFile = "GeneratedWindowsKernelTrace.mof"
+$outputFile = "GeneratedWindowsKernelTrace-Win10.mof"
 
 Write-Host "Discovering kernel trace classes in '$namespace'..."
-
-# 1. Get all classes and build a quick lookup map of child -> parent relationships.
-Write-Host "Building class hierarchy map..."
 $allClasses = Get-CimClass -Namespace $namespace
 $classHierarchy = @{}
-foreach ($class in $allClasses) {
-    $classHierarchy[$class.CimClassName] = $class.CimSuperClassName
-}
+$allClasses | ForEach-Object { $classHierarchy[$_.CimClassName] = $_.CimSuperClassName }
 
-# 2. Find all classes that derive from the base class.
-Write-Host "Finding all classes derived from '$baseClassName'..."
 $kernelClasses = [System.Collections.Generic.List[string]]::new()
-$kernelClasses.Add('EventTrace')       # Add the ultimate base class
-$kernelClasses.Add($baseClassName) # Add the main base class itself
+@('EventTrace', $baseClassName) | ForEach-Object { $kernelClasses.Add($_) }
 
 foreach ($className in $classHierarchy.Keys) {
     $currentClass = $className
-    # Walk up the inheritance chain for the current class
     while ($classHierarchy[$currentClass]) {
         if ($classHierarchy[$currentClass] -eq $baseClassName) {
-            $kernelClasses.Add($className)
-            break # Found it, no need to check further up
+            $kernelClasses.Add($className); break
         }
         $currentClass = $classHierarchy[$currentClass]
     }
 }
 
-# Define a specific order for the base classes, then sort the rest alphabetically.
-$classOrder = @{
-    'EventTrace'       = 1
-    'MSNT_SystemTrace' = 2
-}
-$sortedClassNames = $kernelClasses | Sort-Object -Unique -Property @{Expression = {
-        if ($classOrder.ContainsKey($_)) { $classOrder[$_] } else { 99 }
-    } }, @{Expression = {$_} }
+$classOrder = @{ 'EventTrace' = 1; 'MSNT_SystemTrace' = 2 }
+$sortedClassNames = $kernelClasses | Sort-Object -Unique -Property @{Expression = { if ($classOrder.ContainsKey($_)) { $classOrder[$_] } else { 99 } } }, @{Expression = { $_ } }
 
 Write-Host "Found $($sortedClassNames.Count) kernel trace classes."
-
-# 3. Generate the MOF for each discovered class.
-Write-Host "Generating MOF definitions..."
-$mofDefinitions = @()
+Write-Host "Generating raw MOF definitions..."
+$rawMofDefinitions = @()
 foreach ($className in $sortedClassNames) {
     try {
-        Write-Progress -Activity "Generating MOF" -Status "Processing $className" -PercentComplete (($mofDefinitions.Count * 100) / $sortedClassNames.Count)
-        $mof = Get-MofRepresentation -ClassName $className -Namespace $namespace
-        $mofDefinitions += $mof
+        Write-Progress -Activity "Generating MOF" -Status "Processing $className"
+        $mof = Get-MofRepresentationRaw -ClassName $className -Namespace $namespace
+        $rawMofDefinitions += $mof
     }
     catch {
         Write-Warning "Could not generate MOF for class '$className': $_"
     }
 }
 
-# 4. Combine all definitions and write to the output file.
+Write-Host "Formatting MOF content..."
+$rawContent = $rawMofDefinitions -join "`n`n"
+$formattedContent = Format-MofContent -MofContent $rawContent
+
 $header = "// Auto-generated by mofgen.ps1 on $(Get-Date)`r`n"
 $header += "// Found $($sortedClassNames.Count) classes derived from $baseClassName`r`n`r`n"
-$finalContent = $header + ($mofDefinitions -join "`r`n`r`n")
+$finalContent = $header + $formattedContent
 
 Set-Content -Path $outputFile -Value $finalContent -Encoding UTF8
-
 Write-Host "Successfully generated '$outputFile'"
 
 # Example Usage:
-# Get-MofRepresentation -ClassName "FileIo_V2_MapFile"
-# Get-MofRepresentation -ClassName "Process_V4_TypeGroup1"
+# .\mofgen.ps1
+# Get-MofRepresentationRaw -ClassName "FileIo_V2_MapFile"
+# Get-MofRepresentationRaw -ClassName "Process_V4_TypeGroup1"
